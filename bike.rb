14ccc082288d89c756d79772cf8a8e7993303df3
erinszabo <55733370+erinszabo@users.noreply.github.com>
2021-12-07 14:34:41 -0800

    create bike class

---
+++ bike.rb
@@ -0,0 +1,13 @@
+# sample class to practice with
+
+class Bike
+
+  attr_reader :price, :color
+
+  def initialize(price, color)
+    @price = price
+    @color = color
+  end
+
+
+end
---
+++ main.rb
@@ -0,0 +1,5 @@
+require_relative 'bike'
+
+bike = Bike.new(5, "pink")
+color = bike.color
+puts color
