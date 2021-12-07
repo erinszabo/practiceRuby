8a64c098319ffde5a3bd95678d4a16bd492b977d
erinszabo <55733370+erinszabo@users.noreply.github.com>
2021-12-07 15:47:36 -0800

    add resources to main to reference

--- main.rb
+++ main.rb
@@ -1,3 +1,29 @@
+#### begin/rescue block  ####
+# begin
+#   bike.prepare
+# rescue => exception 
+#   puts 'Uhh, boss, I dunno how to do that.'
+# end
+
+####  ITERATOR  ####
+# collection.each do |variable|
+#    code
+# end
+
+# a = [1,2,3,4,5]
+# b = Array.new
+# b = a.collect
+# puts b
+
+# a = [1,2,3,4,5]
+# b = a.collect{|x| 10*x}
+# puts b
+
+
+
+
+#############################
+
 require_relative 'bike'
 
 bike = Bike.new(5, "pink")
